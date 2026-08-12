d8ffabc40348351ae7514040eb2fc894e4820413
aryan-65548 <aryan.patel25jan@gmail.com>
2026-08-12 12:48:54 +0530

    design auth schema and learned about  zod

---
+++ server/src/schemas/auth.schema.js
@@ -0,0 +1,10 @@
+import { z } from "zod";
+export const registerSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    role: z.enum(["CANDIDATE", "RECRUITER"]).default("CANDIDATE"),
+})
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+});
